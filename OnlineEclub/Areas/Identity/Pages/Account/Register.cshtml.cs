041bc66839e84af969343f6a7aa46a04d5be447c
jack f perry jr <jack.franklin.perryjr@gmail.com>
2018-11-18 14:57:49 -0500

    Added UserName to register page

--- OnlineEclub/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ OnlineEclub/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -40,6 +40,11 @@ namespace OnlineEclub.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [StringLength(25, MinimumLength = 6)]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -66,7 +71,7 @@ namespace OnlineEclub.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
