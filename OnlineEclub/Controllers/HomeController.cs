7a6d759b8f449c0dd6bc905763e39d4bb2d2ec19
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2018-12-01 14:44:17 -0500

    Added skeleton dashboard.

--- OnlineEclub/Controllers/HomeController.cs
+++ OnlineEclub/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OnlineEclub.Models;
 
@@ -10,7 +11,28 @@ namespace OnlineEclub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return View("Dashboard");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Dashboard()
         {
             return View();
         }
